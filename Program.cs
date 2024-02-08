f8ef6a4d403308893a9805ac828b83c2894b6a21
amago.269624 <amago.269624@LAB-A-PC40>
2024-02-08 10:43:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name:");
+            string name = Console.ReadLine();
+            Console.Write("Enter the total number of your courses:");
+            int courses;
+            courses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the price of your favorite book:");
+            double price = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Name:" + name);
+            Console.WriteLine("Total enrolled courses:" + courses);
+            Console.WriteLine("Price of my favorite book:" + price);
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();                                           
+        }
+    }
+}
